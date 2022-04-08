012149b3b786d37c87af269b55b7f95ce0993517
sirmaxford <sirmaxford@gmail.com>
2022-04-08 23:26:24 +0900

    Function

---
+++ Exer/b_functions/target/release/b_functions.d
@@ -0,0 +1,1 @@
+/home/sam/Documents/RUST/Udemy/Ultimate-Rust/Exer/b_functions/target/release/b_functions: /home/sam/Documents/RUST/Udemy/Ultimate-Rust/Exer/b_functions/src/main.rs
---
+++ Exer/b_functions/target/release/deps/b_functions-a3aecb2f42d1cbff.d
@@ -0,0 +1,5 @@
+/home/sam/Documents/RUST/Udemy/Ultimate-Rust/Exer/b_functions/target/release/deps/b_functions-a3aecb2f42d1cbff: src/main.rs
+
+/home/sam/Documents/RUST/Udemy/Ultimate-Rust/Exer/b_functions/target/release/deps/b_functions-a3aecb2f42d1cbff.d: src/main.rs
+
+src/main.rs:
