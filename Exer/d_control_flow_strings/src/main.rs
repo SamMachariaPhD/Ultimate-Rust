e29da6c7ebd270c13b09157162ca5c10fb5b27c0
sirmaxford <sirmaxford@gmail.com>
2022-04-12 23:32:49 +0900

    control flow

--- Exer/d_control_flow_strings/src/main.rs
+++ Exer/d_control_flow_strings/src/main.rs
@@ -20,7 +20,14 @@ fn main() {
         // - If arg is "double", then call the double() function
         // - If arg is anything else, then call the count() function, passing "arg" to it.
 
-        sum();
+        if arg == "sum"{
+            sum();
+        } else if arg == "double"{
+            double();
+        } else {
+            count(arg);
+        }
+                
 
         // 1b. Now try passing "sum", "double" and "bananas" to the program by adding your argument
         // after "cargo run".  For example "cargo run sum"
@@ -32,10 +39,10 @@ fn sum() {
     // 2. Use a "for loop" to iterate through integers from 7 to 23 *inclusive* using a range
     // and add them all together (increment the `sum` variable).  Hint: You should get 255
     // Run it with `cargo run sum`
+
     for num in 7..=23{
         sum = sum+num;
     }
-
 
     println!("The sum is {}", sum);
 }
@@ -47,6 +54,10 @@ fn double() {
     // by 2) until `x` is larger than 500.  Increment `count` each time through the loop. Run it
     // with `cargo run double`  Hint: The answer is 9 times.
 
+    while x<=500{
+        x=x*2;
+        count+=1;
+    }
 
     println!("You can double x {} times until x is larger than 500", count);
 }
@@ -55,8 +66,12 @@ fn count(arg: String) {
     // Challenge: Use an unconditional loop (`loop`) to print `arg` 8 times, and then break.
     // You will need to count your loops, somehow.  Run it with `cargo run bananas`
     //
-    // print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
-
-
+    let mut cnt = 0;
+    loop{
+        if cnt == 8 {break}
+        print!("{} ", arg); // Execute this line 8 times, and then break. `print!` doesn't add a newline.
+        cnt+=1;
+    }
+    
     println!(); // This will output just a newline at the end for cleanliness.
 }
