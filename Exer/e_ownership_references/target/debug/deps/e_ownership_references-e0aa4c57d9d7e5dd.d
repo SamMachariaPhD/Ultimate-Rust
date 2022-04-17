e7c66e962d1eb7fe7b716116281ee37caefe7989
sirmaxford <sirmaxford@gmail.com>
2022-04-17 11:37:27 +0900

    immutable ref exer

---
+++ Exer/e_ownership_references/target/debug/deps/e_ownership_references-e0aa4c57d9d7e5dd.d
@@ -0,0 +1,5 @@
+/home/sam/Documents/RUST/Udemy/Ultimate-Rust/Exer/e_ownership_references/target/debug/deps/e_ownership_references-e0aa4c57d9d7e5dd: src/main.rs
+
+/home/sam/Documents/RUST/Udemy/Ultimate-Rust/Exer/e_ownership_references/target/debug/deps/e_ownership_references-e0aa4c57d9d7e5dd.d: src/main.rs
+
+src/main.rs:
---
+++ Exer/e_ownership_references/target/debug/e_ownership_references.d
@@ -0,0 +1,1 @@
+/home/sam/Documents/RUST/Udemy/Ultimate-Rust/Exer/e_ownership_references/target/debug/e_ownership_references: /home/sam/Documents/RUST/Udemy/Ultimate-Rust/Exer/e_ownership_references/src/main.rs
