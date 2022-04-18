53a9293451b2f3b5d9b81923e926a74551bd65ee
sirmaxford <sirmaxford@gmail.com>
2022-04-18 10:12:16 +0900

    referencing

--- Exer/e_ownership_references/src/main.rs
+++ Exer/e_ownership_references/src/main.rs
@@ -20,7 +20,7 @@ fn main() {
     // the String if it doesn't already end with "s". Then uncomment and run the code below with
     // `cargo run apple`.  Hint: use `.push_str("s")` on the mutable String reference to add an "s".
     //
-    //change(&mut arg);
+    change(&mut arg);
     //println!("I have many {}", arg);
 
     // 3. Write a function `eat` that accepts ownership of (consumes) a String and returns a bool
@@ -47,5 +47,13 @@ fn inspect(arg: &String){
         println!("Plural");
     } else {
         println!("Singular");
+    }
+}
+
+fn change(arg: &mut String){
+    if !arg.ends_with("s"){
+        println!("I have many {:?}", arg.push_str("s"));
+    } else {
+        println!("I have many {}", arg); // the output is just "I have many ()". Why just "()" and not the actual `arg`?
     }
 }
