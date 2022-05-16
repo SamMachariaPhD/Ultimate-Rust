4e6c23b5c9e80facedff157023c9468321efb76b
sirmaxford <sirmaxford@gmail.com>
2022-05-16 09:28:05 +0900

    threading

---
+++ Exer/thread_test1/target/release/deps/thread_test1-a356b0ff8ecfccc5.d
@@ -0,0 +1,5 @@
+/home/sam/Documents/RUST/Udemy/Ultimate-Rust/Exer/thread_test1/target/release/deps/thread_test1-a356b0ff8ecfccc5: src/main.rs
+
+/home/sam/Documents/RUST/Udemy/Ultimate-Rust/Exer/thread_test1/target/release/deps/thread_test1-a356b0ff8ecfccc5.d: src/main.rs
+
+src/main.rs:
---
+++ Exer/thread_test1/target/release/thread_test1.d
@@ -0,0 +1,1 @@
+/home/sam/Documents/RUST/Udemy/Ultimate-Rust/Exer/thread_test1/target/release/thread_test1: /home/sam/Documents/RUST/Udemy/Ultimate-Rust/Exer/thread_test1/src/main.rs
