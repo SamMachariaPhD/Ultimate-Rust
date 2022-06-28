42221e12d67cff9f8040a3b353c9925c7a5d5324
sirmaxford <sirmaxford@gmail.com>
2022-06-28 23:53:51 +0900

    user input for sqrt

--- Exer/sqrt_fxn/src/main.rs
+++ Exer/sqrt_fxn/src/main.rs
@@ -1,3 +1,4 @@
+use std::io; 
 
 static NO:f64 = 5.0;
 
@@ -6,7 +7,14 @@ fn sq(sth: f64) -> f64{
 }
 
 fn main() {
-    println!("SQroot of {:.4} = {:.4}", NO, sq(NO));
+    let mut n_ = String::new();
+    
+    println!("Enter a no: ");
+    io::stdin().read_line(&mut n_).expect("Not a valid string.");
+    let no:f64 = n_.trim().parse().expect("Not a valid number.");
+
+    println!("SQroot of {:.4} = {:.4}", no, sq(no));
+    
     let sh = sq(NO);
-    println!("SQroot = {}", sh);
+    println!("`static` SQroot = {}", sh);
 }
