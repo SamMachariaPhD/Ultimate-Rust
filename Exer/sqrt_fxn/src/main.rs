78d90c67e99129aa9445ca75c4410d9972656d6b
sirmaxford <sirmaxford@gmail.com>
2022-06-30 06:06:23 +0900

    user input for sqrt and abs unused var

--- Exer/sqrt_fxn/src/main.rs
+++ Exer/sqrt_fxn/src/main.rs
@@ -1,13 +1,13 @@
 use std::io; 
 
-static NO:f64 = 5.0;
+static NO:f32 = 5.0;
 
-fn sq(sth: f64) -> f64{
+fn sq(sth: f32) -> f32{
     sth*sth // `return sth*sth;` also works but rarely used
 }
 
 // absolute value of a floating-point number
-fn abs(x: f64) -> f64 {
+fn abs(x: f32) -> f32 {
     if x > 0.0 {
         x
     } else {
@@ -20,10 +20,11 @@ fn main() {
     
     println!("Enter a no: ");
     io::stdin().read_line(&mut n_).expect("Not a valid string.");
-    let no:f64 = n_.trim().parse().expect("Not a valid number.");
+    let no:f32 = n_.trim().parse().expect("Not a valid number.");
 
     println!("SQroot of {:.4} = {:.4}", no, sq(no));
+    println!("The abs val of {} is {}.", no, abs(no));
     
-    let sh = sq(NO);
-    println!("`static` SQroot = {}", sh);
+    let _sh = sq(NO); // `_sth` for unused/unimplemented variable. `_` alone has less information about the variable name.
+    //println!("`static` SQroot = {}", sh);
 }
