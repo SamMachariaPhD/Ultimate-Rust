1dc789f13fcc7428aa4e03c2e8930e5205977aca
sirmaxford <sirmaxford@gmail.com>
2022-07-17 23:36:15 +0900

    test ls

--- Exer/tests/src/main.rs
+++ Exer/tests/src/main.rs
@@ -5,4 +5,13 @@
 #[test]
 fn works(){
     assert!(true); // what about `!true`?
+} 
+
+use std::process::Command;
+
+#[test]
+fn runs(){
+    let mut cmd = Command::new("ls");
+    let res = cmd.output();
+    assert!(res.is_ok());
 }
