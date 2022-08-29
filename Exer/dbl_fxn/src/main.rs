9ee8f7c0924872988ae6854eaf2c0916fc2edf6d
sirmaxford <sirmaxford@gmail.com>
2022-08-29 11:54:09 +0300

    double arr elemts

--- Exer/dbl_fxn/src/main.rs
+++ Exer/dbl_fxn/src/main.rs
@@ -1,11 +1,16 @@
 
 fn main() {
+    // let mut ar = [2,3,4,5,6];
+    // for n in 0..ar.len(){
+    //     ar[n] *= 2;
+    // }
+    // println!("{:?}", ar);
     let arr = [1,2,3,5,9,4,2,8,5,6];
     println!("Single {:?}\nDouble {:?}", arr, double(arr));
 }
 
 fn double(mut a:[i32;10]) -> [i32;10]{
-    for i in 0..10 {
+    for i in 0..a.len() {
         a[i] *= 2;
     }
     a
