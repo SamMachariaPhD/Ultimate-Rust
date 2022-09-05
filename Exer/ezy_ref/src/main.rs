9567b172ab527f62af7919ed3bc1c0bc3058ccbe
sirmaxford <sirmaxford@gmail.com>
2022-09-05 23:46:35 +0300

    print ref of ref

--- Exer/ezy_ref/src/main.rs
+++ Exer/ezy_ref/src/main.rs
@@ -2,7 +2,22 @@
 
 fn main() {
     let n = 3;
+    let x = &&&5;
     let ref_n = &n;
 
     println!("Number n = {}\nRef n (&n,*ref_n) = {}\nRef n (&n,ref_n) = {}", n, *ref_n, ref_n);
+    /*
+    output:
+    Number n = 3
+    Ref n (&n,*ref_n) = 3
+    Ref n (&n,ref_n) = 3
+    */
+    println!("***x = {}\n**x = {}\n*x = {}\nx = {}", ***x, **x, *x, x);
+    /*
+    output:
+    ***x = 5    the object referenced by "x" is obtained using the right most "*"
+    **x = 5     the object is a reference of reference
+    *x = 5      the object is a reference and it is taken using "*"
+    x = 5       the object is a number 5
+    */
 }
